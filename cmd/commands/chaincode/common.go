b6fbb3dcc53078a42ce5d1a08757908faacd8b3b
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2020-07-24 16:00:35 -0400

    [FAB-18094] Updated to the latest fabric-sdk-go and fabric-protos
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- cmd/commands/chaincode/common.go
+++ cmd/commands/chaincode/common.go
@@ -7,7 +7,8 @@ import (
 
 	"github.com/hyperledger/fabric-protos-go/common"
 	pb "github.com/hyperledger/fabric-protos-go/peer"
-	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
+
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/policydsl"
 )
 
 type collectionConfigJSON struct {
@@ -41,7 +42,7 @@ func getCollectionsConfigFromBytes(bytes []byte) ([]*pb.CollectionConfig, error)
 
 	ccarray := make([]*pb.CollectionConfig, 0, len(cconf))
 	for _, cconfitem := range cconf {
-		p, err := cauthdsl.FromString(cconfitem.Policy)
+		p, err := policydsl.FromString(cconfitem.Policy)
 		if err != nil {
 			return nil, err
 		}
@@ -67,10 +68,10 @@ func getCollectionsConfigFromBytes(bytes []byte) ([]*pb.CollectionConfig, error)
 
 func getChaincodePolicy(policyString string) (*common.SignaturePolicyEnvelope, error) {
 	if len(policyString) == 0 {
-		return cauthdsl.AcceptAllPolicy, nil
+		return policydsl.AcceptAllPolicy, nil
 	}
 
-	policy, err := cauthdsl.FromString(policyString)
+	policy, err := policydsl.FromString(policyString)
 	if err != nil {
 		return nil, errors.New("error parsing chaincode policy")
 	}
